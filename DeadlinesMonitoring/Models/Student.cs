154c6e8aeeada9c4641489873ad2a373d0465654
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-05 21:23:32 +0700

    Color item

--- DeadlinesMonitoring/Models/Student.cs
+++ DeadlinesMonitoring/Models/Student.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Avalonia.Media;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DeadlinesMonitoring.Models
 {
-    public class Student
+    public class Student : INotifyPropertyChanged
     {
         public Student() {}
         public string TextFIOCS { get; set; }
@@ -15,5 +17,18 @@ namespace DeadlinesMonitoring.Models
         public string TextComputerScienceCS { get; set; }
         public string TextSocialScienceCS { get; set; }
         public string TextAverageCS { get; set; }
+
+
+        public SolidColorBrush colorPhysicsCS = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ColorPhysicsCS
+        {
+            get => colorPhysicsCS;
+            set
+            {
+                colorPhysicsCS = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColorPhysicsCS)));
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
