7c2d7c95a1681ba60da91fbf6d9f6a4640b8b1b6
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-05 21:42:00 +0700

    Color zap

--- DeadlinesMonitoring/Models/Student.cs
+++ DeadlinesMonitoring/Models/Student.cs
@@ -5,10 +5,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DeadlinesMonitoring.Models
 {
-    public class Student : INotifyPropertyChanged
+    public class Student// : INotifyPropertyChanged
     {
         public Student() {}
         public string TextFIOCS { get; set; }
@@ -18,17 +19,84 @@ namespace DeadlinesMonitoring.Models
         public string TextSocialScienceCS { get; set; }
         public string TextAverageCS { get; set; }
 
-
-        public SolidColorBrush colorPhysicsCS = new SolidColorBrush(Colors.Red);
-        public SolidColorBrush ColorPhysicsCS
+        public SolidColorBrush? colorPhysicsCS = null;
+        public SolidColorBrush? ColorPhysicsCS
         {
-            get => colorPhysicsCS;
+            get
+            {
+                if (float.Parse(TextPhysicsCS) < 1) return new SolidColorBrush(Colors.Red);
+                else if (float.Parse(TextPhysicsCS) == 1) return new SolidColorBrush(Colors.Yellow);
+                else if (float.Parse(TextPhysicsCS) >= 1) return new SolidColorBrush(Colors.Green);
+                return null;
+            }
             set
             {
                 colorPhysicsCS = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ColorPhysicsCS)));
             }
         }
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public SolidColorBrush colorHistoryCS = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ColorHistoryCS
+        {
+            get
+            {
+                if (float.Parse(TextHistoryCS) < 1) return new SolidColorBrush(Colors.Red);
+                else if (float.Parse(TextHistoryCS) == 1) return new SolidColorBrush(Colors.Yellow);
+                else if (float.Parse(TextHistoryCS) >= 1) return new SolidColorBrush(Colors.Green);
+                return null;
+            }
+            set
+            {
+                colorHistoryCS = value;
+                
+            }
+        }
+        public SolidColorBrush colorComputerScienceCS = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ColorComputerScienceCS
+        {
+            get
+            {
+                if (float.Parse(TextComputerScienceCS) < 1) return new SolidColorBrush(Colors.Red);
+                else if (float.Parse(TextComputerScienceCS) == 1) return new SolidColorBrush(Colors.Yellow);
+                else if (float.Parse(TextComputerScienceCS) >= 1) return new SolidColorBrush(Colors.Green);
+                return null;
+            }
+            set
+            {
+                colorComputerScienceCS = value;
+
+            }
+        }
+        public SolidColorBrush colorSocialScienceCS = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ColorSocialScienceCS
+        {
+            get
+            {
+                if (float.Parse(TextSocialScienceCS) < 1) return new SolidColorBrush(Colors.Red);
+                else if (float.Parse(TextSocialScienceCS) == 1) return new SolidColorBrush(Colors.Yellow);
+                else if (float.Parse(TextSocialScienceCS) >= 1) return new SolidColorBrush(Colors.Green);
+                return null;
+            }
+            set
+            {
+                colorSocialScienceCS = value;
+
+            }
+        }
+        public SolidColorBrush colorAverageCS = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ColorAverageCS
+        {
+            get
+            {
+                if (float.Parse(TextAverageCS) < 1) return new SolidColorBrush(Colors.Red);
+                else if (float.Parse(TextAverageCS) == 1) return new SolidColorBrush(Colors.Yellow);
+                else if (float.Parse(TextAverageCS) >= 1) return new SolidColorBrush(Colors.Green);
+                return null;
+            }
+            set
+            {
+                colorAverageCS = value;
+
+            }
+        }
     }
 }
