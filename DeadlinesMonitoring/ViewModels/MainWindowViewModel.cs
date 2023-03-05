f349192ada5f185140246f20d01bfb49b4123405
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-05 11:05:17 +0700

    Markup, variables

--- DeadlinesMonitoring/ViewModels/MainWindowViewModel.cs
+++ DeadlinesMonitoring/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,14 @@ namespace DeadlinesMonitoring.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
+        public string physicsAverageCS;
+        public string historyAverageCS;
+        public string computerScienceAverageCS;
+        public string socialScienceAverageCS;
+        public string physicsCS;
+        public string historyCS;
+        public string computerScienceCS;
+        public string socialScienceCS;
     }
 }
