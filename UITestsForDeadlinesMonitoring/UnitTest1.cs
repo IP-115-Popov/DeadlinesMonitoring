80e0fc7f5068a57ae406d72dc81ad067ce8bf48b
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-18 22:21:39 +0700

    Add test Fill_listBox_1_item1

--- UITestsForDeadlinesMonitoring/UnitTest1.cs
+++ UITestsForDeadlinesMonitoring/UnitTest1.cs
@@ -11,21 +11,33 @@ namespace UITestsForDeadlinesMonitoring
         {
             var app = AvaloniaApp.GetApp();
             var mainWindow = AvaloniaApp.GetMainWindow();
-            //var ExpectedText = "Попов Сергей";
+
+            int Expected = 1;
 
             await Task.Delay(100);
             var B_Add = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "buttonAdd");
-            //var textbox = mainWindow.GetVisualDescendants().OfType<TextBox>().First(t => t.Name == "fIOCS");
-
-            //textbox.Text = "Попов Сергей";
-            //B_Add.Command.Execute(B_Add.CommandParameter);
-
             var listBox = mainWindow.GetVisualDescendants().OfType<ListBox>().First(t => t.Name == "studentList");
-            //var listBoxItems = listBox.GetVisualDescendants().OfType<ListBoxItem>().First();
 
-            // var rez = listBoxItems.;
+            Assert.True(listBox.ItemCount == Expected);
+            await Task.Delay(50);
+        }
+        [Fact]
+        public async void buttonAdd1()
+        {
+            var app = AvaloniaApp.GetApp();
+            var mainWindow = AvaloniaApp.GetMainWindow();
+            var ExpectedText = "Попов Сергей";
+
+            await Task.Delay(100);
+            var B_Add = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "buttonAdd");
+            var textbox = mainWindow.GetVisualDescendants().OfType<TextBox>().First(t => t.Name == "fIOCS");
+
+            textbox.Text = "Попов Сергей";
             B_Add.Command.Execute(B_Add.CommandParameter);
-            Assert.True(listBox.ItemCount == 1);
+
+            var textfio = mainWindow.GetVisualDescendants().OfType<TextBlock>().First(t => t.Name == "itemFIO").Text;
+
+            Assert.True(textfio == "Попов Сергей");
 
             await Task.Delay(50);
         }
